694a5c07aced39bcfcc15eec2750b62d7f6df8ac
Fernando Jinzenji <fernandoj@me.com>
2018-11-13 11:13:46 -0800

    initial commmit

---
+++ PhotosInKeychain/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PhotosInKeychain
+//
+//  Created by Fernando Jinzenji on 2018-11-13.
+//  Copyright © 2018 Fernando Jinzenji. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PhotosInKeychain/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhotosInKeychain
+//
+//  Created by Fernando Jinzenji on 2018-11-13.
+//  Copyright © 2018 Fernando Jinzenji. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
